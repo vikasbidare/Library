2671f9926bf9c53f041a8626ac64dac4b29aafc6
vikasbidare <mailme.vikasb@gmail.com>
2019-10-26 12:50:09 +0530

    Intents in edit info

--- app/src/main/java/com/example/library/Login.java
+++ app/src/main/java/com/example/library/Login.java
@@ -71,8 +71,5 @@ public class Login extends AppCompatActivity {
                 startActivity(intentToForgotPassword);
             }
         });
-
-
-
     }
 }
--- app/src/main/java/com/example/library/editinfo.java
+++ app/src/main/java/com/example/library/editinfo.java
@@ -2,7 +2,10 @@ package com.example.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class editinfo extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class editinfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editinfo);
+
+        Button editstudent = (Button) findViewById(R.id.editstudentbtn);
+        editstudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in1 = new Intent(editinfo.this,editstudentinfo.class);
+                startActivity(in1);
+            }
+        });
+
+        Button editfaculty = (Button) findViewById(R.id.editfacultybtn);
+        editstudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in2 = new Intent(editinfo.this,editfacultyinfo.class);
+                startActivity(in2);
+            }
+        });
     }
+
+
+
+
 }
