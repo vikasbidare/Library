9e58d206904631f037f89482e8c8415d572bbc71
rcs10 <dhanueshrc28@gmail.com>
2019-10-26 03:42:16 -0700

    showhistory partially created

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             items.add(new DataClass(R.drawable.return_book,"Return Book",new Intent(MainActivity.this,return_from_both.class)));
             items.add(new DataClass(R.drawable.edit_info,"Edit Info",new Intent(MainActivity.this,editinfo.class)));
             items.add(new DataClass(R.drawable.show_book,"Show Book",new Intent(MainActivity.this,show_book.class) ));
-            items.add(new DataClass(R.drawable.history,"Show History",new Intent(MainActivity.this,add_book.class) ));
+            items.add(new DataClass(R.drawable.history,"Show History",new Intent(MainActivity.this,show_history.class) ));
 
 
         }
