04b3dca53c70ad3a8427640b1d160a5488a69d70
rcs10 <dhanueshrc28@gmail.com>
2019-10-26 00:08:02 -0700

    Process in returnfaculty implemented

--- app/src/main/java/com/example/library/returnfaculty.java
+++ app/src/main/java/com/example/library/returnfaculty.java
@@ -2,7 +2,13 @@ package com.example.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class returnfaculty extends AppCompatActivity {
 
@@ -10,5 +16,45 @@ public class returnfaculty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_returnfaculty);
+
+
+        final LinearLayout facultyFineLayout = (LinearLayout) findViewById(R.id.returnFacultyLayout);
+        if(facultyFineLayout.getVisibility()== View.VISIBLE||facultyFineLayout.getVisibility()==View.INVISIBLE)
+            facultyFineLayout.setVisibility(View.GONE);
+
+        final TextView facultyFineAmt = (TextView) findViewById(R.id.facultyFineAmt);
+        facultyFineAmt.setEnabled(false);
+        facultyFineAmt.setText("0.00");
+
+        Button finecalculator = (Button) findViewById(R.id.facultyProcessbtn);
+        finecalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(facultyFineLayout.getVisibility()==View.GONE||facultyFineLayout.getVisibility()==View.INVISIBLE)
+                {
+                    facultyFineLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        Button returnfacultycancel = (Button) findViewById(R.id.returnFacultyCancelBtn);
+        Button returnfacultysubmit = (Button) findViewById(R.id.returnFacultytSubmitBtn);
+
+        final Intent intentToMain = new Intent(returnfaculty.this,MainActivity.class);
+        returnfacultycancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(returnfaculty.this, "Submission Cancelled", Toast.LENGTH_SHORT).show();
+                startActivity(intentToMain);
+            }
+        });
+
+        returnfacultysubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(returnfaculty.this, "Submission Sucessfull", Toast.LENGTH_SHORT).show();
+                startActivity(intentToMain);
+            }
+        });
     }
 }
