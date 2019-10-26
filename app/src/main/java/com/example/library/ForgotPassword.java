579ab348025526dc0177d4bc476393f1a8c6119b
rcs10 <dhanueshrc28@gmail.com>
2019-10-25 22:04:08 -0700

    forgotPassword page added

---
+++ app/src/main/java/com/example/library/ForgotPassword.java
@@ -0,0 +1,41 @@
+package com.example.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        ProgressBar signUpProgressBar = (ProgressBar) findViewById(R.id.resetPasswordProgressBar);
+        signUpProgressBar.setVisibility(View.GONE);
+
+        Button passwordresetbtn = (Button) findViewById(R.id.resetPasswordBtn);
+        passwordresetbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentToLogin = new Intent(ForgotPassword.this,Login.class);
+                startActivity(intentToLogin);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        ProgressBar signUpProgressBar = (ProgressBar) findViewById(R.id.signUpProgressBar);
+
+        if(signUpProgressBar.getVisibility() == View.VISIBLE ||signUpProgressBar.getVisibility() == View.INVISIBLE)
+            signUpProgressBar.setVisibility(View.GONE);
+    }
+}
--- app/src/main/java/com/example/library/Login.java
+++ app/src/main/java/com/example/library/Login.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class Login extends AppCompatActivity {
 
     @Override
@@ -31,6 +33,15 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intentToMainActivity = new Intent(Login.this,MainActivity.class);
                 startActivity(intentToMainActivity);
+            }
+        });
+
+        TextView forgotPassword = (TextView) findViewById(R.id.loginforgotpassword);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentToForgotPassword = new Intent(Login.this,ForgotPassword.class);
+                startActivity(intentToForgotPassword);
             }
         });
     }
