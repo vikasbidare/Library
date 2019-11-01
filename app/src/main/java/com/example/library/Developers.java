eacbe9311f2e398b0ee563a17cc5323e21e90837
rcs <dhanueshrc28@gmail.com>
2019-11-01 19:35:26 +0530

    developers implemented

--- app/src/main/java/com/example/library/Developers.java
+++ app/src/main/java/com/example/library/Developers.java
@@ -2,7 +2,12 @@ package com.example.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Developers extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class Developers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developers);
+
+        TextView rcs1 = (TextView) findViewById(R.id.developersLinkedIn1);
+        rcs1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent linkedinIntent = new Intent(Intent.ACTION_VIEW);
+                linkedinIntent.setClassName("com.linkedin.android", "com.linkedin.android.profile.ViewProfileActivity");
+                linkedinIntent.putExtra("memberId", "dhanuesh-r-c-167478172");
+                linkedinIntent.setPackage("com.linkedin.android");
+                try {
+                    startActivity(linkedinIntent);
+                } catch ( ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/dhanuesh-r-c-167478172")));
+                }
+            }
+        });
     }
 }
+
+
--- app/src/main/java/com/example/library/profile_settings.java
+++ app/src/main/java/com/example/library/profile_settings.java
@@ -24,7 +24,7 @@ public class profile_settings extends AppCompatActivity {
             items.add(new DataClass(R.drawable.editaccount,"EditAccount",new Intent(profile_settings.this,add_book.class) ));
             items.add(new DataClass(R.drawable.share,"ShareApp",new Intent(profile_settings.this,add_student.class) ));
             items.add(new DataClass(R.drawable.terms,"Directions",new Intent(profile_settings.this,terms.class) ));
-            items.add(new DataClass(R.drawable.developers,"Developers",new Intent(profile_settings.this,issue_to_both.class) ));
+            items.add(new DataClass(R.drawable.developers,"Developers",new Intent(profile_settings.this,Developers.class) ));
             items.add(new DataClass(R.drawable.delete,"DeleteAccount",new Intent(profile_settings.this,issue_to_both.class) ));
         }
         ViewAdapter adapter = new ViewAdapter(profile_settings.this, items);
