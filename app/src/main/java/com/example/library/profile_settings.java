b75714a12619f7d06fd2224bc823bcaeb13789b4
rcs <dhanueshrc28@gmail.com>
2019-11-01 16:46:37 +0530

    shareapp without link implemented

--- app/src/main/java/com/example/library/profile_settings.java
+++ app/src/main/java/com/example/library/profile_settings.java
@@ -19,10 +19,17 @@ public class profile_settings extends AppCompatActivity {
 
         ArrayList<DataClass> items = new ArrayList<>();
 
+        Intent toShareApp = new Intent();
+        toShareApp.setAction(Intent.ACTION_SEND);
+        toShareApp.putExtra(Intent.EXTRA_TEXT,
+                "Try out the new LibraryApp for easing the functionality of librarian \n Download soon from the given link \n " +
+                        "https://drive.google.com/file/d/1wxD4O1aIuxjL2jSkj3Uk5xyiZfxZbXGX/view?usp=sharing");
+        toShareApp.setType("text/plain");
+        Intent.createChooser(toShareApp, "Share app via");
 
         if (items.size() == 0) {
             items.add(new DataClass(R.drawable.editaccount,"EditAccount",new Intent(profile_settings.this,add_book.class) ));
-            items.add(new DataClass(R.drawable.share,"ShareApp",new Intent(profile_settings.this,add_student.class) ));
+            items.add(new DataClass(R.drawable.share,"ShareApp",toShareApp));
             items.add(new DataClass(R.drawable.terms,"Directions",new Intent(profile_settings.this,add_faculty.class) ));
             items.add(new DataClass(R.drawable.developers,"Developers",new Intent(profile_settings.this,issue_to_both.class) ));
             items.add(new DataClass(R.drawable.delete,"DeleteAccount",new Intent(profile_settings.this,issue_to_both.class) ));
